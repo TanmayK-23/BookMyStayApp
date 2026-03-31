f5628e152595ae36196569e90763901c5e777d85
Tanmay <kumartanmay39@gmail.com>
2026-03-31 10:35:42 +0530

    UC5: Booking request queue using FIFO

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -271,3 +271,90 @@ class UseCase4RoomSearch {
         service.searchAvailableRooms(inventory, single, dbl, suite);
     }
 }
+
+/**
+ * ================================================================
+ * CLASS - Reservation
+ * ================================================================
+ *
+ * Use Case 5: Booking Request (FIFO)
+ *
+ * @version 5.0
+ */
+class Reservation {
+
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public String getGuestName() { return guestName; }
+
+    public String getRoomType() { return roomType; }
+}
+
+/**
+ * ================================================================
+ * CLASS - BookingRequestQueue
+ * ================================================================
+ *
+ * Use Case 5: Booking Request (FIFO)
+ *
+ * @version 5.0
+ */
+class BookingRequestQueue {
+
+    private java.util.Queue<Reservation> requestQueue;
+
+    public BookingRequestQueue() {
+        requestQueue = new java.util.LinkedList<>();
+    }
+
+    public void addRequest(Reservation reservation) {
+        requestQueue.offer(reservation);
+    }
+
+    public Reservation getNextRequest() {
+        return requestQueue.poll();
+    }
+
+    public boolean hasPendingRequests() {
+        return !requestQueue.isEmpty();
+    }
+}
+
+/**
+ * ================================================================
+ * MAIN CLASS - UseCase5BookingRequestQueue
+ * ================================================================
+ *
+ * Use Case 5: Booking Request (First-Come-First-Served)
+ *
+ * @version 5.0
+ */
+class UseCase5BookingRequestQueue {
+
+    public static void main(String[] args) {
+
+        System.out.println("Booking Request Queue");
+
+        BookingRequestQueue bookingQueue = new BookingRequestQueue();
+
+        Reservation r1 = new Reservation("Abhi", "Single");
+        Reservation r2 = new Reservation("Subha", "Double");
+        Reservation r3 = new Reservation("Vanmathi", "Suite");
+
+        bookingQueue.addRequest(r1);
+        bookingQueue.addRequest(r2);
+        bookingQueue.addRequest(r3);
+
+        while (bookingQueue.hasPendingRequests()) {
+            Reservation r = bookingQueue.getNextRequest();
+            System.out.println("Processing booking for Guest: " + r.getGuestName()
+                    + ", Room Type: " + r.getRoomType());
+        }
+    }
+}
