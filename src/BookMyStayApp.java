dbd21f0053d634823017f3fb1a323e421bc9ee9e
Tanmay <kumartanmay39@gmail.com>
2026-03-31 10:24:18 +0530

    UC2: Basic room types and static availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -40,3 +40,94 @@ public class BookMyStayApp {
 
     }
 }
+
+/**
+ * ================================================================
+ * ABSTRACT CLASS - Room
+ * ================================================================
+ *
+ * Use Case 2: Basic Room Types & Static Availability
+ *
+ * @version 2.0
+ */
+abstract class Room {
+
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails(int available) {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+        System.out.println("Available: " + available);
+        System.out.println();
+    }
+}
+
+/**
+ * CLASS - SingleRoom
+ * @version 2.0
+ */
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
+
+/**
+ * CLASS - DoubleRoom
+ * @version 2.0
+ */
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+/**
+ * CLASS - SuiteRoom
+ * @version 2.0
+ */
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
+
+/**
+ * ================================================================
+ * MAIN CLASS - UseCase2RoomInitialization
+ * ================================================================
+ *
+ * @version 2.0
+ */
+class UseCase2RoomInitialization {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hotel Room Initialization\n");
+
+        SingleRoom single = new SingleRoom();
+        DoubleRoom dbl = new DoubleRoom();
+        SuiteRoom suite = new SuiteRoom();
+
+        System.out.println("Single Room:");
+        single.displayRoomDetails(5);
+
+        System.out.println("Double Room:");
+        dbl.displayRoomDetails(3);
+
+        System.out.println("Suite Room:");
+        suite.displayRoomDetails(2);
+    }
+}
